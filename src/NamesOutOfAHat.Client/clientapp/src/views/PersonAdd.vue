4a99653c8d983d4997d77b9c664e740aeec8bbd9
Ben Osborne <osborne.ben@gmail.com>
2021-10-28 11:31:45 -0500

    edit functionality working

--- src/NamesOutOfAHat.Client/clientapp/src/views/PersonAdd.vue
+++ src/NamesOutOfAHat.Client/clientapp/src/views/PersonAdd.vue
@@ -1,27 +1,20 @@
 <template>
-    <div class="card" style="width: 18rem;display: inline-flex;margin: 10px;">
-
+    <div class="card mb-3" style="width: 18rem;display: inline-flex;margin: 10px;">
+        <h5 v-if="person.name.trim().length === 0" class="card-header">Add Person</h5>
+        <h5 v-if="person.name.trim().length > 0" class="card-header">{{ person.name }}</h5>
         <div class="card-body">
-            <h5 v-if="person.name.length === 0" class="card-title">Add Person</h5>
-            <h5 v-if="person.name.length > 0" class="card-title">{{ person.name }}</h5>
-
             <div class="form-floating mb-3">
                 <input type="text" class="form-control" v-model="person.name" />
                 <label for="Name">Name</label>
             </div>
-
             <div class="form-floating mb-3">
                 <input type="email" class="form-control" v-model="person.email" />
                 <label for="Email">Email</label>
             </div>
-
-            <a href="#" class="btn btn-primary" @click="commit" >Add</a>&nbsp;
-            <a href="#" class="btn btn-danger" @click="cancel" >Cancel</a>
-
+            <a href="#" class="btn btn-sm btn-primary" @click="commit" >Add</a>&nbsp;
+            <a href="#" class="btn btn-sm btn-danger" @click="cancel" >Cancel</a>
         </div>
-
     </div>
-
 </template>
 
 <script lang="ts" >
--- src/NamesOutOfAHat.Client/clientapp/src/views/PersonDisplay.vue
+++ src/NamesOutOfAHat.Client/clientapp/src/views/PersonDisplay.vue
@@ -1,13 +1,27 @@
 <template>
     <div class="card mb-3" style="width: 18rem;display: inline-flex;margin: 10px;">
-      <h5 class="card-header">{{ person.name }}</h5>
-      <ul class="list-group list-group-flush">
+      <h5 v-if="person.name.trim().length === 0" class="card-header">Edit Person</h5>
+      <h5 v-if="person.name.trim().length > 0" class="card-header">{{ person.name }}</h5>
+      <!-- display mode stuff -->
+      <ul v-if="!editMode" class="list-group list-group-flush">
         <li v-if="person.email.length > 0" class="list-group-item">📧 {{ person.email }}</li>
         <li class="list-group-item">
-          <a href="#" class="btn btn-sm btn-primary">Edit</a>&nbsp;
+          <a href="#" class="btn btn-sm btn-primary" @click="editMode = true" >Edit</a>&nbsp;
           <a href="#" class="btn btn-sm btn-danger" @click="remove" >Delete</a>
         </li>
       </ul>
+      <!-- edit mode stuff -->
+      <div v-if="editMode" class="card-body">
+        <div class="form-floating mb-3">
+          <input type="text" class="form-control" v-model="person.name" />
+          <label for="Name">Name</label>
+        </div>
+        <div class="form-floating mb-3">
+          <input type="email" class="form-control" v-model="person.email" />
+          <label for="Email">Email</label>
+        </div>
+        <a href="#" class="btn btn-sm btn-primary" @click="editMode = false" >Close</a>
+      </div>
     </div>
 </template>
 
@@ -20,6 +34,9 @@ import Person from '@/components/Person.vue'
     person: Person,
     removeFunction: (person: Person) => Boolean
   },
+  data: () => ({
+    editMode: Boolean(false)
+  }),
   methods: {
     remove () {
       this.removeFunction(this.person)
